437d46eba2f4a6249b6f488c074d5a5233d73aa4
swade-wh40k <doodlitude@gmail.com>
2024-11-26 15:45:18 +0300

    zzz...

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+if [ -f package.json ]; then
+  bash -i -c "nvm install --lts && nvm install-latest-npm"
+  npm i
+  npm run build
+fi
+
+# Add OMZ plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+sed -i -E "s/^(plugins=\()(git)(\))/\1\2 zsh-syntax-highlighting zsh-autosuggestions\3/" ~/.zshrc
+
+# Avoid git log use less
+echo -e "\nunset LESS" >>~/.zshrc
+
+bundle exec jekyll serve
