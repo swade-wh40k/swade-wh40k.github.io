8b2d221d0cd090f209ddd7ecfdb98fd7d85a9b1c
swade-wh40k <doodlitude@gmail.com>
2024-11-29 11:30:55 +0300

    beauty - no link to ship - safe animation (can't stuck) - better spaces

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,13 @@ collection = document.getElementsByClassName('animScaleFadeIn');
 
 for (var i = 0; i < collection.length; i++) {
     (function(i) {
+        collection[i].style.opacity = "0%";
+        collection[i].style.transform = "scale(0.5)";
         setTimeout(function() {
-        collection[i].style.opacity = 1;
-        collection[i].style.transform = "scale(1)";
+            collection[i].style.transitionDuration = "0.25s";
+            collection[i].style.transitionTimingFunction = "ease-out";
+            collection[i].style.opacity = 1;
+            collection[i].style.transform = "scale(1)";
         }, i * 50);
     })(i);
 }
